eaeae0c6ab46db1e59ad5d9c0284e238822fa6ea
Harrison <harrison-johnson@outlook.com>
2023-05-10 14:41:12 +0100

    Implement userdetailsserviceimpl methods

--- gameLink/src/main/java/com/ab/repositories/UserAccountRepository.java
+++ gameLink/src/main/java/com/ab/repositories/UserAccountRepository.java
@@ -1,11 +1,16 @@
 package com.ab.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.ab.entities.UserAccount;
 
 @Repository
 public interface UserAccountRepository extends JpaRepository<UserAccount, Integer>{
+
+	@Query(value = "select new UserAccount(ua.accountId, ua.username, ua.email, ua.password, ua.firstName, ua.lastName) from UserAccount ua where ua.username = :username")
+	public UserAccount findByUsername(@Param("username") String username);
 
 }
--- gameLink/src/main/java/com/ab/services/UserDetailsServiceImpl.java
+++ gameLink/src/main/java/com/ab/services/UserDetailsServiceImpl.java
@@ -1,11 +1,14 @@
 package com.ab.services;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.ab.entities.MyUserDetails;
+import com.ab.entities.UserAccount;
 import com.ab.entities.UserData;
 import com.ab.exceptions.UserAlreadyExistsException;
 import com.ab.repositories.UserAccountRepository;
@@ -21,20 +24,39 @@ public class UserDetailsServiceImpl implements MyUserDetailsService {
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+
+		UserAccount user = userAccountRepository.findByUsername(username);
+		if (user == null) {
+			throw new UsernameNotFoundException("Could not find user");
+		}
+		
+		return new MyUserDetails(user);
 	}
 
 	@Override
 	public void register(UserData user) throws UserAlreadyExistsException {
-		// TODO Auto-generated method stub
+		
+		if (checkIfUserExist(user.getUsername())) {
+			throw new UserAlreadyExistsException("User already exists for this username");
+		}
+		
+		UserAccount userEntity = new UserAccount();
+		BeanUtils.copyProperties(user, userEntity);
+		encodePassword(userEntity, user);
+		
+		userAccountRepository.save(userEntity);
 		
 	}
 
 	@Override
 	public boolean checkIfUserExist(String username) {
-		// TODO Auto-generated method stub
-		return false;
+
+		return userAccountRepository.findByUsername(username) != null ? true : false;
+		
+	}
+	
+	private void encodePassword(UserAccount userEntity, UserData user) {
+		userEntity.setPassword(passwordEncoder.encode(user.getPassword()));
 	}
 	
 	
